f82da11aede4333a16e20965a5077c44a58cc351
SamyaRoy999 <samyaroy999@gmail.com>
2025-04-12 20:01:10 +0600

    stor data AsyncStorage

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,8 +30,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name='index' />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
--- app/index.tsx
+++ app/index.tsx
@@ -5,15 +5,25 @@ import {
   Image,
   TouchableOpacity,
   Text,
+  KeyboardAvoidingView,
+  Keyboard,
 } from "react-native";
-import React, { Component } from "react";
+import React, {  useEffect, useState } from "react";
 import Entypo from "@expo/vector-icons/Entypo";
 import Feather from "@expo/vector-icons/Feather";
 import { TextInput } from "react-native";
 import { FlatList } from "react-native";
 import Checkbox from "expo-checkbox";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 // import avaterImg from '../assets/images/alexander-hipp-iEEBWgY_6lA-unsplash.jpg'
+
+type todoType = {
+  id: number;
+  title: string,
+  isDone: boolean,
+}
 export default function index() {
   const todoData = [
     {
@@ -54,12 +64,52 @@ export default function index() {
     },
   ];
 
+  const [todos, setTodos]= useState<todoType[]>(todoData);
+  const [todosText, setTodosText]= useState<string>();
+
+// get todos async store
+useEffect(() => {
+  const getData = async () => {
+    try {
+      const todos = await AsyncStorage.getItem('my-key');
+      if (todos !== null) {
+        setTodos(JSON.parse(todos))
+      }
+    } catch (e) {
+      // error reading value
+    }
+  };
+  getData()
+}, [])
+
+
+
+// set async store update data
+  const addTodo = async() =>{
+    try{
+
+      const newTodo = {
+        id: Math.random(),
+        title: todosText,
+        isDone: false,
+      }  
+      const updatedTodos = [...todos, newTodo];
+      setTodosText('')
+      Keyboard.dismiss();
+      await AsyncStorage.setItem('my-key', JSON.stringify(updatedTodos));
+    }catch (e) {
+      console.log('====================================');
+      console.log(e);
+      console.log('====================================');
+    }
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.headers}>
         <TouchableOpacity
           onPress={() => {
-            alert("Clicked");
+            // alert("Clicked");
           }}
         >
           <Entypo name="menu" size={24} color="black" />
@@ -67,7 +117,7 @@ export default function index() {
         <TouchableOpacity
           onPress={() => {
             alert("Clicked");
-          }}
+          }} 
         >
           <Image
             style={styles.avaterImg}
@@ -83,21 +133,33 @@ export default function index() {
 
       <FlatList
         style={styles.todoContainer}
-        data={todoData}
+        data={todos.reverse()}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
-          <View style={styles.todoBoxContainer}>
-            <View style={styles.todoBox}>
-              <Checkbox value={item.isDone} />
-              <Text>{item.title}</Text>
-            </View>
-            <AntDesign name="delete" size={24} color="red" />
-          </View>
+         <TodoItem todo={item} />
         )}
       />
+      <KeyboardAvoidingView style={styles.footer}>
+        <TextInput value={todosText} onChangeText={(text)=>  setTodosText(text)} style={styles.addInput} placeholder="Add new todo" />
+        <TouchableOpacity style={styles.addButton} onPress={()=> addTodo()}>
+          <Ionicons name="add" size={24} color="#fff" />
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
+
+const TodoItem = ({todo}: {todo: todoType}) =>(
+  <View style={styles.todoBoxContainer}>
+  <View style={styles.todoBox}>
+    <Checkbox value={todo.isDone} />
+    <Text>{todo.title}</Text>
+  </View>
+  <TouchableOpacity onPress={() => alert("delete" + todo.id)}>
+    <AntDesign name="delete" size={24} color="red" />
+  </TouchableOpacity>
+</View>
+)
 
 const styles = StyleSheet.create({
   container: {
@@ -119,7 +181,7 @@ const styles = StyleSheet.create({
   searchBar: {
     flexDirection: "row",
     backgroundColor: "#fff",
-    padding: 16,
+    padding: 15,
     borderRadius: 10,
     gap: 10,
     alignItems: "center",
@@ -132,13 +194,36 @@ const styles = StyleSheet.create({
   },
   todoBox: {
     flexDirection: "row",
-    margin: 4
+    margin: 4,
   },
-  todoBoxContainer:{
-    flexDirection: 'row',
+  todoBoxContainer: {
+    flexDirection: "row",
     backgroundColor: "#eaeaea",
     padding: 10,
     margin: 10,
-    justifyContent: 'space-between'
-  }
+    justifyContent: "space-between",
+  },
+  // button
+  footer: {
+    flexDirection: "row",
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    marginTop: 10,
+    marginBottom: 10,
+    borderRadius: 10
+     
+  },
+  addInput: {
+    marginTop: 15,
+    marginBottom: 15,
+    borderRadius: 10,
+    flex: 1,
+    padding: 5,
+    margin: 2
+
+  },
+  addButton: {
+    backgroundColor: '#9ae19d',
+    padding: 15,
+  },
 });
